46fb1582b5379777e39925315edb4c1391ddf215
Ajith k v <ajith66310@gmail.com>
2026-01-27 16:13:42 +0530

    Add routing and new pages for About, Careers, Contact, Learn, Services, Works, and Error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Careers from './pages/Careers'
+import Contact from './pages/Contact'
+import Learn from './pages/Learn'
+import Services from './pages/Services'
+import Works from './pages/Works'
+import Error from './pages/Error'
 
 const App = () => {
   return (
     <>
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/careers' element={<Careers/>} />
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/learn' element={<Learn/>} />
+      <Route path='/services' element={<Services/>} />
+      <Route path='/works' element={<Works/>} />
+      <Route path='*' element={<Error/>} />
     </Routes>
     </>
   )
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+      <p>brand-name</p>
+    </div>
+    <div>
+      <ul>
+        <li>About</li>
+        <li>Services</li>
+        <li>Works</li>
+        <li>Learn</li>
+        <li>Careers</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+    <div>
+      <button>Get a proposal</button>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
