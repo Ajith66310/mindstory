c697d3ac03c3a0449b5d2ea07cd341f870a0750a
Ajith k v <ajith66310@gmail.com>
2026-01-29 12:02:20 +0530

    GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/mindstory/frontend",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
